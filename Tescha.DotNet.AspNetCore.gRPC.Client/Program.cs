5c9e733e4e4743ae7d70a45bb05e9dacfb2d05f4
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2021-04-23 04:55:46 +1000

    gRPC server and client apps to connect on mac.

---
+++ Tescha.DotNet.AspNetCore.gRPC.Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Teshca.DotNet.AspNetCore.gRPC.Client;
+
+namespace Tescha.DotNet.AspNetCore.gRPC.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+           
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:50050");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "Greeter Client" } );
+
+            Console.WriteLine($"Reply from gRPC GreeterService {reply.Message}");
+        }
+    }
+}
---
+++ Teshca.DotNet.AspNetCore.gRPC/Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Hosting;
+
+namespace Teshca.DotNet.AspNetCore.gRPC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+             CreateHostBuilder(args).Build().Run();
+        }
+
+        // Additional configuration is required to succ
+        //
+        // essfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    //https://www.fabiocozzolino.eu/HTTP2-over-TLS-is-not-supported-on-macOS/
+                    //https://github.com/grpc/grpc-dotnet/issues/416
+                    // Uncomment below configuration to disblae TLS on macOS.
+                    // Use it only for development environment.
+                    // Make sure to keep it commented on production environment.
+                    //.ConfigureKestrel(options =>
+                    //{
+                    //    options.Limits.MinRequestBodyDataRate = null;
+
+                    //    options.ListenAnyIP(50050,
+                    //          listenOptions => {
+                    //              listenOptions.Protocols = HttpProtocols.Http1;
+                    //              listenOptions.UseHttps();
+                    //          });
+
+                    //    options.ListenAnyIP(50051,
+                    //          listenOptions => {
+                    //              listenOptions.Protocols = HttpProtocols.Http2;
+                    //          });
+                    //});
+                });
+    }
+}
